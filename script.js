ec2aae416659b8d2fc114cdf62cde21079e866a9
rameshchandramali05 <rameshchandramali05@gmail.com>
2026-07-04 00:59:29 +0530

    Add load function to fetch and display HTML content

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ fetch("home.html")
   .then(html => {
     document.getElementById("home").innerHTML = html;
   });
+
+
+
+function load(file, id){
+    fetch(file)
+      .then(res => res.text())
+      .then(html => {
+          document.getElementById(id).innerHTML = html;
+      });
+}
+
+load("header.html","header");
+load("welcom.html","welcom");
+load("add.html","add");
+load("readmore.html","readmore");
+load("Func.html","Func");
+load("sadasy.html","sadasy");
